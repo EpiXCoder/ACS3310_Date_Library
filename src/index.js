591c5e9ae26c03405a922d4b754039e962c206f7
Vithusha Ravirajan <vithusharavirajan@Vithus-MBP.local>
2023-09-04 20:59:41 -0400

    challenge 2 complete

--- src/index.js
+++ src/index.js
@@ -1,19 +1,73 @@
 class D {
-    constructor(...args){
-        this._date = new Date(...args)
-    }
+	constructor(...args) {
+		this._date = new Date(...args);
+	}
+
+	// Full Year
+	get year() {
+		return this._date.getFullYear();
+	}
+
+	// Short Year
+	get yr() {
+		return this._date.getFullYear() % 100;
+	}
+
+	// Full Month
+	get month() {
+		const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+		return monthNames[this._date.getMonth()];
+	}
+
+	// Short Month
+	get mon() {
+		const shortMonthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+		return shortMonthNames[this._date.getMonth()];
+	}
+
+	// Full Day
+	get day() {
+		const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+		return dayNames[this._date.getDay()];
+	}
+
+	// Short Day
+	get dy() {
+		const shortDayNames = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+		return shortDayNames[this._date.getDay()];
+	}
+
+	// Date of the month
+	get date() {
+		return this._date.getDate();
+	}
+
+	// Hour
+	get hours() {
+		return this._date.getHours();
+	}
+
+	// Minutes
+	get mins() {
+		return this._date.getMinutes();
+	}
+
+	// Seconds
+	get secs() {
+		return this._date.getSeconds();
+	}
 }
 
-// With no parameters: 
-const a = new D()
-// Create a date from a string (single parameter)
-const b = new D('9/26/1965') 
-// Create a date from some numbers (list of parameters)
-const c = new D(1970, 1, 1, 0, 0, 0)
-// With a Date
-const d = new D(new Date())
 
-console.log(a)
-console.log(b)
-console.log(c)
-console.log(d)
+// Sample usage:
+const d = new D();
+console.log(d.year);  // Full year
+console.log(d.yr);    // Short year
+console.log(d.month); // Full month name
+console.log(d.mon);   // Short month name
+console.log(d.day);   // Full day name
+console.log(d.dy);    // Short day name
+console.log(d.date);  // Day of the month
+console.log(d.hours); // Current hour
+console.log(d.mins);  // Current minutes
+console.log(d.secs);  // Current seconds
